4c98442204a1467f5f931f256ce15845b74f2f5e
ggangman <115800583+ggangman@users.noreply.github.com>
2022-10-26 11:18:39 +0900

    Add files via upload

---
+++ task2model/MovieClassification.py
@@ -0,0 +1,20 @@
+from model.transformer import *
+import torch.nn as nn
+
+class MovieClassification(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = Transformer(self.config)
+        self.projection = nn.Linear(self.config.d_hidn, self.config.n_output, bias = False)
+
+    def forward(self, enc_inputs, dec_inputs):
+        # (bs, n_dec_seq, d_hidn), [(bs, n_head, n_enc_seq, n_enc_seq)], [(bs, n_head, n_dec_seq, n_dec_seq)], [(bs, n_head, n_dec_seq, n_enc_seq)]
+        dec_outputs, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs = self.transformer(enc_inputs, dec_inputs)
+        # (bs, d_hidn)
+        dec_outputs, _ = torch.max(dec_outputs, dim=1)
+        # (bs, n_output)
+        logits = self.projection(dec_outputs)
+        # (bs, n_output), [(bs, n_head, n_enc_seq, n_enc_seq)], [(bs, n_head, n_dec_seq, n_dec_seq)], [(bs, n_head, n_dec_seq, n_enc_seq)]
+        return logits, enc_self_attn_probs, dec_self_attn_probs, dec_enc_attn_probs
